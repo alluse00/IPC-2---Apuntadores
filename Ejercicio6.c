1cca41963861b88efbb526d7128490a6e84b6914
Allan J. Rafael <allanmorales05@gmail.com>
2021-02-04 08:35:03 -0600

    Ejercicios de 4 a 7

---
+++ Ejercicio6.c
@@ -0,0 +1,8 @@
+int main() {
+    int *ptr_int;
+    float *ptr_float;
+    int value;
+    ptr_int = &value;
+    ptr_float = &value;
+    return 0;
+}
---
+++ Ejercicio7.c
@@ -0,0 +1,6 @@
+int main() {
+    float *ptr_float;
+    printf("El valor apuntado por ptr_float es %f", *ptr_float);
+    *ptr_float = 10.5;
+    return 0;
+}
